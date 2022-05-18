f770897914b104f1a678369ae97f47fbeda6e087
RamyaNuthalapati <ramyanuthalapati7@gmail.com>
2022-05-18 16:23:29 +0530

    final-commit

--- config/config.py
+++ config/config.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import boto3
+
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
+app=Flask(__name__,static_url_path="")
+
+client = boto3.client('s3', 
+         region_name='us-west-2',
+         aws_access_key_id=os.getenv('ACCESS_KEY'),
+         aws_secret_access_key=os.getenv('SECRET_KEY')
+      )
+
 
 
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ["DB_URL"]
